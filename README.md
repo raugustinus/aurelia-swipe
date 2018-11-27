1beea20dbfe3db5c480129efc8ee45f24fa07c52
Rob Augustinus <rob@opensolutions.nl>
2018-11-27 23:01:52 +0100

    correct handling of the touchend event

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # aurelia-swipe sample
 
 Sample project to try out some mobile swipe functionality.
+
+## Interesting links
+
+ + https://stackoverflow.com/questions/2264072/detect-a-finger-swipe-through-javascript-on-the-iphone-and-android
+ + https://getbootstrap.com/docs/4.1/components/carousel/#carouseloptions
+ + http://hammerjs.github.io/
+ 
--- src/components/swipe.ts
+++ src/components/swipe.ts
@@ -1,6 +1,9 @@
 export class Swipe {
 
-  swipeTrack;
+  aantal:number = 0;
+  startx:number = 0;
+  endx:number = 0;
+
   msg:string = 'nothing yet..';
 
   attached() {
@@ -11,33 +14,35 @@ export class Swipe {
 
     let elems:HTMLCollectionOf<Element> = document.getElementsByClassName('carousel');
     for (let i=0;i<elems.length;i++) {
-
       let el:Element = elems.item(i);
 
-      el.addEventListener('touchstart', (e:TouchEvent) => {
-        e.preventDefault();
-        this.msg = `can't touch this.`;
-        for(let i=0;i<e.touches.length;i++) {
-          this.msg = `x = ${e.touches.item(i).clientX}`;
-        }
-      }, false);
-
-      el.addEventListener('touchend', e => {
-        e.preventDefault();
-        this.msg = `touchend..`;
-      });
-
-      el.addEventListener('touchcancel', e => {
-        e.preventDefault();
-        this.msg = 'touch cancelled..';
+      el.addEventListener('touchend', (e:TouchEvent) => {
+        this.endx = e.changedTouches[0].clientX;
+        this.aantal = e.touches.length;
+        this.endx = e.touches[1].clientX;
+        this.msg = `touchend -> aantal touches: ${e.touches.length}`;
       });
 
       el.addEventListener('touchmove', (e:TouchEvent) => {
         this.msg = `we've been moved..`;
         for(let i=0;i<e.touches.length;i++) {
-          this.msg = `x = ${e.touches.item(i).clientX}`;
+          this.msg = `move x = ${e.touches.item(i).clientX}`;
         }
       }, false);
+
+      el.addEventListener('touchstart', (e:TouchEvent) => {
+        e.preventDefault();
+        this.aantal = e.touches.length;
+        this.startx = e.touches[0].clientX;
+        // for(let i=0;i<e.touches.length;i++) {
+        //   this.msg = `start x = ${e.touches.item(i).clientX}`;
+        // }
+      }, false);
+
+      el.addEventListener('touchcancel', e => {
+        e.preventDefault();
+        this.msg = 'touch cancelled..';
+      });
     }
   }
 }
