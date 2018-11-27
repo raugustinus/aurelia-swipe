20926322f202fa9e2896cf17f09641671950e784
Rob Augustinus <rob@opensolutions.nl>
2018-11-27 23:20:19 +0100

    basic swipe detection works

--- src/components/swipe.ts
+++ src/components/swipe.ts
@@ -1,8 +1,20 @@
+class SimpleTouch {
+  x:number;
+  when:number;
+  constructor(x:number) {
+    this.x = x;
+    this.when = Date.now();
+  }
+}
+
 export class Swipe {
 
   aantal:number = 0;
   startx:number = 0;
   endx:number = 0;
+
+  start:SimpleTouch = null;
+  end:SimpleTouch = null;
 
   msg:string = 'nothing yet..';
 
@@ -19,30 +31,42 @@ export class Swipe {
       el.addEventListener('touchend', (e:TouchEvent) => {
         this.endx = e.changedTouches[0].clientX;
         this.aantal = e.touches.length;
-        this.endx = e.touches[1].clientX;
         this.msg = `touchend -> aantal touches: ${e.touches.length}`;
+
+        this.end = new SimpleTouch(this.endx);
+        this.msg = 'are we detecting !??';
+        this.detectSwipe(this.start.x, this.end.x);
       });
 
-      el.addEventListener('touchmove', (e:TouchEvent) => {
-        this.msg = `we've been moved..`;
-        for(let i=0;i<e.touches.length;i++) {
-          this.msg = `move x = ${e.touches.item(i).clientX}`;
-        }
-      }, false);
+      // el.addEventListener('touchmove', (e:TouchEvent) => {
+      //   this.msg = `we've been moved..`;
+      //   for(let i=0;i<e.touches.length;i++) {
+      //     this.msg = `move x = ${e.touches.item(i).clientX}`;
+      //   }
+      // }, false);
 
       el.addEventListener('touchstart', (e:TouchEvent) => {
         e.preventDefault();
         this.aantal = e.touches.length;
         this.startx = e.touches[0].clientX;
-        // for(let i=0;i<e.touches.length;i++) {
-        //   this.msg = `start x = ${e.touches.item(i).clientX}`;
-        // }
+        this.start = new SimpleTouch(e.touches[0].clientX); // don't care about your other fingers in e.touches
       }, false);
 
       el.addEventListener('touchcancel', e => {
         e.preventDefault();
         this.msg = 'touch cancelled..';
       });
+    }
+  }
+
+  public detectSwipe(pointA:number, pointB:number) {
+    this.msg = 'detecting...';
+    if (pointA == pointB) {
+      this.msg = `That's impossible!?!`;
+    } else if (pointA > pointB) { // swipe left
+      this.msg = `Detected swipe LEFT!`;
+    } else if (pointA < pointB) { // swipe right
+      this.msg = `Detected swipe RIGHT!`;
     }
   }
 }
